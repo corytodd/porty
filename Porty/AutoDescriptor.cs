e910b5d994b4453893fa6e1ce7cf1f6e25e75495
Cory Todd <corneliustodd@gmail.com>
2015-04-13 20:54:42 -0700

    autodescriptors are actions to be executed on the detection of a certain device.

---
+++ Porty/AutoDescriptor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Porty
+{
+    class AutoDescriptor
+    {
+        /// <summary>
+        /// Gets or sets the PID/VID associated with this auto descriptor.
+        /// Any devices detected with this PID/VID will use this descriptor file
+        /// </summary>
+        public string PidVid { get; set; }
+
+        /// <summary>
+        /// Execture each of these commands
+        /// </summary>
+        public List<string> CommandSet { get; set; }
+            
+    }
+}
